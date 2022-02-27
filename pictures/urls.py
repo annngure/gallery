e6641545a07fd9f2254b82a993777dd7c9000a59
annngure <annwanjikungure@gmail.com>
2022-02-27 23:03:33 +0300

    update

--- pictures/urls.py
+++ pictures/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.index,name= 'index'),
-    re_path(r'^search/',views.search_results, name='search_results')
+    re_path(r'^search/',views.search_results, name='search_results'),
+    re_path(r'^location/(\d+)',views.filter_by_location,name='location')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
