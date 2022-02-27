3d3c7f983bfad006dd53a2ac52806c9a21d3d791
annngure <annwanjikungure@gmail.com>
2022-02-27 16:16:28 +0300

    add models

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Photographer(models.Model):
+    first_name =models.CharField(max_length =30 )
+    last_name = models.CharField(max_length= 30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    
+    def save_Photographer(self):
+        self.save()
+
+class Meta:
+        odering = ['first_name']
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def save_Location(self):
+        self.save()
+
+    def delete_Location(self):
+        self.delete()
+
+
+class Category(models.Model):
+    name= models.CharField(max_length = 30)
+
+    def save_Category(self):
+        self.save()
+
+    def delete_Category(self):
+        self.delete()
+
+
+
+
+
--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Photographer, Location, Category
+
 
 # Create your tests here.
+
+class PhotographerTest
